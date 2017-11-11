6217a1ba017a169634438cb3c933d30a7798e1e5
Kamil Koczur <kamil.koczur@pb.com>
2017-11-11 16:11:58 +0100

    added entities

---
+++ src/main/java/org/jhipster/blog/domain/Blog.java
@@ -0,0 +1,142 @@
+package org.jhipster.blog.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Objects;
+
+/**
+ * A Blog.
+ */
+@Entity
+@Table(name = "blog")
+public class Blog implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(name = "handle", nullable = false)
+    private String handle;
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany(mappedBy = "blog")
+    @JsonIgnore
+    private Set<Entry> entries = new HashSet<>();
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Blog name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHandle() {
+        return handle;
+    }
+
+    public Blog handle(String handle) {
+        this.handle = handle;
+        return this;
+    }
+
+    public void setHandle(String handle) {
+        this.handle = handle;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Blog user(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Set<Entry> getEntries() {
+        return entries;
+    }
+
+    public Blog entries(Set<Entry> entries) {
+        this.entries = entries;
+        return this;
+    }
+
+    public Blog addEntry(Entry entry) {
+        this.entries.add(entry);
+        entry.setBlog(this);
+        return this;
+    }
+
+    public Blog removeEntry(Entry entry) {
+        this.entries.remove(entry);
+        entry.setBlog(null);
+        return this;
+    }
+
+    public void setEntries(Set<Entry> entries) {
+        this.entries = entries;
+    }
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Blog blog = (Blog) o;
+        if (blog.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), blog.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Blog{" +
+            "id=" + getId() +
+            ", name='" + getName() + "'" +
+            ", handle='" + getHandle() + "'" +
+            "}";
+    }
+}
--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,15 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { BlogBlogModule } from './blog/blog.module';
+import { BlogTagModule } from './tag/tag.module';
+import { BlogEntryModule } from './entry/entry.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        BlogBlogModule,
+        BlogTagModule,
+        BlogEntryModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
